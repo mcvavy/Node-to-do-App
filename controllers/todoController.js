7a7e5dff4d8409c004d626eab9c5ba2bef1af576
Michael Oyibo <codingrealms@gmail.com>
2018-03-18 15:37:07 +0000

    implemented post request using AJAX

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,22 @@
+const bodyParser = require('body-parser');
+
+let urlEncodedParser = bodyParser.urlencoded({ extended: false });
+
+let data = [
+    { item: 'get milk' },
+    { item: 'walk dog' },
+    { item: 'kick some coding ass' }
+];
+
 module.exports = (app) => {
 
     app.get('/todo', (req, res) => {
-        res.render('todo');
+        res.render('todo', { todos: data });
     });
 
-    app.post('/todo', (req, res) => {
-
+    app.post('/todo', urlEncodedParser, (req, res) => {
+        data.push(req.body);
+        res.json(data);
     });
 
     app.delete('/todo', (req, res) => {
